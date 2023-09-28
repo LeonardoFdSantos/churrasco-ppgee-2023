27294fef48ee6d47defb939a8d660d82b6fb53b3
Leonardo Felipe dos Santos <leosan95@live.com>
2023-09-28 18:52:09 -0300

    atualizando automaticamente

--- src/components/thermometer/index.js
+++ src/components/thermometer/index.js
@@ -2,76 +2,50 @@ import Thermometer from 'react-thermometer-component';
 
 import { useEffect, useState } from 'react';
 
-const thermos = [
-    {
-        id: 1,
-        Name: "GEPOC",
-        value: 0,
-        valueMax: 30, 
-    },
-    {
-        id: 2,
-        Name: "GEDRE",
-        value: 0,
-        valueMax: 18, 
-    },
-    {
-        id: 3,
-        Name: "CEESP",
-        value: 0,
-        valueMax: 18, 
-    },
-  ]
-  
-  export default function Page() {
-    const [data, setData] = useState;
+export default function Page() {
+  const defaultValue = []
+  const [data, setData] = useState(defaultValue);
 
-    const getData = async () => {
-      try {
-        const res = await fetch('https://sheet.best/api/sheets/b58fc9f2-2a3d-42b4-b079-9cafc4a27cf3');
-        const data = await res.json();
-        setData(data);
-      } catch (error) {
-        console.log(error)
-      }
+  const getApiData = async () =>{
+    const response = await fetch("https://sheet.best/api/sheets/b58fc9f2-2a3d-42b4-b079-9cafc4a27cf3").then((response) => response.json());
+    setData(response);
+  }
 
-    }
+  useEffect(() => {
+    getApiData();
+  }, []);
 
-    useEffect(() => {
-      getData();
-    }, []);
-
-    return (
-      <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Pagamentos</h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-              Pagamentos realizados para o Churrasco do PPGEE 2023
-            </p>
-          </div>
-          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {data?.map((item)=>(
-                <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href={item.href}>
-                    <span className="absolute inset-0" />
-                    {item.Grupo}
-                  </a>
-                </h3>
-                <Thermometer
-                    theme="light"
-                    value={(item.quantidade_atual/item.valor_maximo)*100}
-                    max="100"
-                    steps="10"
-                    format="%"
-                    size="large"
-                    height="300"
-                />
-              </div>
-            ))}
-          </div>
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Pagamentos</h2>
+          <p className="mt-2 text-lg leading-8 text-gray-600">
+            Pagamentos realizados para o Churrasco do PPGEE 2023
+          </p>
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {data.map((item)=>(
+              <div className="group relative">
+              <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                <a href={item.href}>
+                  <span className="absolute inset-0" />
+                  {item.Grupo}
+                </a>
+              </h3>
+              <Thermometer
+                  theme="light"
+                  value={(item.quantidade_atual/item.valor_maximo)*100}
+                  max="100"
+                  steps="10"
+                  format="%"
+                  size="large"
+                  height="300"
+              />
+            </div>
+          ))}
         </div>
       </div>
-    )
+    </div>
+  )
   }
