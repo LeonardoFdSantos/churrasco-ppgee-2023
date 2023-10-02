7df164ecbc6044dd34bfcdf7cdc57c8530d5a3b2
Leonardo Felipe dos Santos <leosan95@live.com>
2023-10-02 16:14:51 -0300

    Atualizando Tabelas totais - Atualizando Home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,8 +1,56 @@
-export default function Home() {
-    return (
-      <div>
-        <h2>Home View</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
-      </div>
-    );
-  }
+import axios from 'axios';
+
+import { useEffect, useState } from 'react';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+import {
+    Table,
+    Thead,
+    Tbody,
+    Divider,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+  } from '@chakra-ui/react';
+
+import { Card, Text, CardBody } from '@chakra-ui/react'
+
+const baseURL = "https://api.steinhq.com/v1/storages/651605b561eb47055da02529/DashBoard";
+export default function Home() {  
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios.get(baseURL).then((response) => {
+        setData(response.data);
+      });
+    };
+    
+    fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData(); // Fetch data every 2 minutes
+    }, 600000);
+
+    return () => clearInterval(intervalId);
+
+  }, []);
+
+  if (!data) return null;
+
+  return (
+    <ChakraProvider>
+        <main>
+            <Card>
+                <CardBody>
+                    <Text>Informações sobre o Churrasco do PPGEE</Text>
+                    <Text>Use o Menu acima para navegação no sistema</Text>
+                </CardBody>
+            </Card>
+        </main>
+    </ChakraProvider>
+  )
+}
