af407ddf8f2afb250d3b0ed35e3712a1df6a1dda
Leonardo Felipe dos Santos <leosan95@live.com>
2023-10-02 16:15:22 -0300

    Atualizando Tabelas totais - Atualizando Home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,46 +1,7 @@
-import axios from 'axios';
-
-import { useEffect, useState } from 'react';
-
 import { ChakraProvider } from '@chakra-ui/react';
-
-import {
-    Table,
-    Thead,
-    Tbody,
-    Divider,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react';
-
 import { Card, Text, CardBody } from '@chakra-ui/react'
 
-const baseURL = "https://api.steinhq.com/v1/storages/651605b561eb47055da02529/DashBoard";
 export default function Home() {  
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await axios.get(baseURL).then((response) => {
-        setData(response.data);
-      });
-    };
-    
-    fetchData();
-
-    const intervalId = setInterval(() => {
-      fetchData(); // Fetch data every 2 minutes
-    }, 600000);
-
-    return () => clearInterval(intervalId);
-
-  }, []);
-
-  if (!data) return null;
-
   return (
     <ChakraProvider>
         <main>
